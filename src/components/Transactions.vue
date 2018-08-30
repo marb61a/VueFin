4e1345c0e4fb74d8d1ad1abe83730c70b1fee36c
marb61a <marb61a@gmail.com>
2018-08-30 17:26:20 +0100

    updated Transactions component form

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -27,8 +27,41 @@
       v-bind:headers="headers"
       v-bind:items="items"
       v-bind:search="search"
-
+      item-key="_id"
     >
+      <template slot="items" slot-scope="props">
+        <tr>
+          <td>
+            <v-edit-dialog
+              lazy
+            >
+              {{ props.item.transactionDate }}
+              <v-text-field
+                slot="input"
+                label="Edit"
+                v-model="props.item.transactionDate"
+                single-line
+              >
+              </v-text-field>
+            </v-edit-dialog>
+          </td>
+          <td class="text-xs-left">{{ props.item.transactionType }}</td>
+          <td class="text-xs-left" @click="props.expanded = !pr.expanded">
+            {{ props.item.description }}
+          </td>
+          <td class="text-xs-right"> {{ props.item.charge }} </td>
+          <td class="text-xs-right"> {{ props.item.deposit }} </td>
+          <td class="text-xs-right"> {{ props.item.balance }} </td>
+        </tr>
+      </template>
+      <template slot="expand" slot-scope="props">
+        <v-card flat class="pl-5">
+          <v-card-text class="text-xs-left">
+            <v-text-field label="Note" v-model="props.item.notes">
+            </v-text-field>
+          </v-card-text>
+        </v-card>
+      </template>
     </v-data-table>
   </v-card>
 </template>
