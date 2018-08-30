db35e3eee6ba83ec21b13d609ce216848f241872
marb61a <marb61a@gmail.com>
2018-08-30 16:37:26 +0100

    updated editTransaction component form

--- src/components/EditTransactions.vue
+++ src/components/EditTransactions.vue
@@ -25,10 +25,42 @@
                 ref="datePicker"
                 lazy
                 :close-on-content-click="false"
-
+                v-model="transactionDatePicker"
+                transition="scale-transition"
+                offset-y
+                full-width
+                :nudge-right="40"
+                max-width="290px"
+                min-width="290px"
               >
-
+                <v-text-field
+                  slot="activator"
+                  label="Select Transaction Date"
+                  v-model="transaction.transactionDate"
+                  prepend-icon="event"
+                  readonly
+                >
+                </v-text-field>
+                <v-date-picker
+                  v-model="transaction.transactionDate"
+                  v-on:input="$refs.datePicker.save(transaction)"
+                >
+                </v-date-picker>
               </v-menu>
+            </v-flex>
+            <v-flex xs12>
+              <v-select
+                prepend-icon="credit_card"
+                v-bind:items="transactionTypes"
+                v-model="transaction.transactionType"
+                label="Transaction Type"
+                single-line
+                bottom
+              >
+              </v-select>
+            </v-flex>
+            <v-flex xs12>
+
             </v-flex>
           </v-layout>
         </v-container>
