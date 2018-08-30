cb93c625fa336ffb122cda8dd2db7d86424eef18
marb61a <marb61a@gmail.com>
2018-08-30 12:11:54 +0100

    Adding form fields to login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>
-    <h2>Login</h2>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h2>Login to VueFin</h2>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          label="Email"
+          v-model="email"
+          v-bind:rules="emailRules"
+          required
+        >
+        </v-text-field>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          v-bind:rules="passwordRules"
+          v-bind:type="'password'"
+          required
+        >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
