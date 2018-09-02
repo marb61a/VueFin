900791d9b3932a7afd48117fc500b03b05687b06
marb61a <marb61a@gmail.com>
2018-09-02 18:51:19 +0100

    updated user store

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,3 +4,40 @@ const state = {
   isLoggedIn: false,
   loginError: ''
 }
+
+const getters = {
+  isLoggedIn: state => state.isLoggedIn,
+  userId: state => state.userId,
+  loginError: state => state.loginError
+}
+
+const actions = {
+  logInUser ({ commit }, payload) {
+    if (payload.email === 'test1@user.com' && payload.password === 'test111') {
+      // Simulate getting back a valid userId from API call...
+      payload.userId = '5a777f0a75f64a1698221d98'
+      commit('logInUser', payload)
+    } else {
+      commit('loginError')
+    }
+  }
+}
+
+const mutations = {
+  logInUser (state, payload) {
+    state.email = payload.email
+    state.userId = payload.userId
+    state.isLoggedIn = true
+  },
+  loginError (state) {
+    state.isLoggedIn = false
+    state.loginError = 'Email and/or Password are invalid. Login failed.'
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
