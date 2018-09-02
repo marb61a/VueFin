89271a927ddb2dbae115327459cb60f68961ef2d
marb61a <marb61a@gmail.com>
2018-09-02 14:23:40 +0100

    updating home component template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,11 +44,54 @@
               <v-list-tile-action v-if="child.icon">
                 <v-icon>{{ child.icon }}</v-icon>
               </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
             </v-list-tile>
           </v-list-group>
+          <v-list-tile v-else @click="menuAction" v-bind:key="ndx">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
         </template>
       </v-list>
     </v-navigation-drawer>
+    <v-toolbar
+      color="blue darken-3"
+      dark
+      app
+      clipped-left
+      fixed
+    >
+      <v-toolbar-title :style="$vuetify.breakpoint.smAndUp ? '' : 'min-width: 72px'" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-xs-only">Globomantics Rewards</span>
+      </v-toolbar-title>
+      <!-- <v-text-field
+        light
+        solo
+        prepend-icon="search"
+        placeholder="Search"
+        style="max-width: 500px; min-width: 128px"
+      ></v-text-field> -->
+      <!-- <header-actions></header-actions> -->
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout>
+            <transactions></transactions>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <edit-transaction></edit-transaction>
   </div>
 </template>
 
