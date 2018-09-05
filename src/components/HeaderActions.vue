7fa52ac316f1c15b70cc25b2a9e84bc1fb665909
marb61a <marb61a@gmail.com>
2018-09-05 11:17:56 +0100

    updated main.js and added initial HeaderActions template

---
+++ src/components/HeaderActions.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex align-center" style="margin-left: auto">
+    <v-menu open-on-hover bottom offset-y>
+      <v-btn icon slot="activtor">
+        <v-icon>person</v-icon>
+      </v-btn>
+      <v-list>
+
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,24 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+
+// Store (Vuex)
+import store from './store'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 
+// Ensure axios is globally available
+Vue.axios = Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:8081/api'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
